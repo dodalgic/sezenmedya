ab9334f355828a755695c9415d6a017b0a31a059
dodalgic <dodalgic@gmail.com>
2026-06-10 20:21:15 +0300

    Add files via upload

---
+++ captcha/x5captcha.php
@@ -0,0 +1,12 @@
+<?php
+include("../res/x5engine.php");
+$nameList = array("k43","k88","zry","vxa","elk","vwz","57z","lwv","gn2","wkr");
+$charList = array("V","N","C","Z","2","4","G","6","6","A");
+$cpt = new X5Captcha($nameList, $charList);
+//Check Captcha
+if ($_GET["action"] == "check")
+	echo $cpt->check($_GET["code"], $_GET["ans"]);
+//Show Captcha chars
+else if ($_GET["action"] == "show")
+	echo $cpt->show($_GET['code']);
+// End of file x5captcha.php
---
+++ imemail/imEmailForm.php
@@ -0,0 +1,29 @@
+<?php
+if(substr(basename($_SERVER['PHP_SELF']), 0, 11) == "imEmailForm") {
+	include '../res/x5engine.php';
+	$form = new ImForm();
+
+	$errorMessage = '';
+	if(@$_POST['action'] != 'check_answer') {
+	$form->setField('Ad Soyad', @$_POST['imObjectForm_13_1'], '', false);
+	$form->setField('E-posta Adresi', @$_POST['imObjectForm_13_2'], '', false);
+	$form->setField('Konu', @$_POST['imObjectForm_13_3'], '', false);
+	$form->setField('Mesajınız', @$_POST['imObjectForm_13_4'], '', false);
+	$form->setField('KVKK Onayı', @$_POST['imObjectForm_13_5'], '', false);
+		if(!isset($_POST['imJsCheck']) || $_POST['imJsCheck'] != '14FC093C396DFE480F5773173566C49B' || (isset($_POST['imSpProt']) && $_POST['imSpProt'] != ""))
+			$errorMessage = "JavaScript'i aktifleştirmelisiniz!";
+		$form->mailToOwner('mail@sezenmedya.com.tr', '', '', 'Yeni iletişim', "Web sitemden yeni veriler alındı:", false);
+		if ($errorMessage == '') {
+			echo "{\"status\" : true}";
+		}
+
+		else {
+			echo "{\"status\" : false, \"err\" : \"$errorMessage\"}";
+		}
+		exit();
+	} else {
+		echo $form->checkAnswer(@$_POST['id'], @$_POST['answer']) ? 1 : 0;
+	}
+}
+
+// End of file
